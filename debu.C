0e28b12d0e0d4280711d41b318ae84841c062d07
DebabrataBhowmik <debabratabhowmik2@gmail.com>
2017-02-18 23:10:48 +0530

    Create debu.C

---
+++ debu.C
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main()
+{
+//This is simple adding code
+int a,b;
+cout<< "Tell me your 1st number"<<endl;
+cin>>a;
+cout<< "Tell me your 2nd number"<<endl;
+cin>>b;
+cout<<"So" <<a "+" << b << "=" << a+b<<endl;
+
+}
